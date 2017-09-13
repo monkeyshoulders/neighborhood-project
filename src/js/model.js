b31758a0ea8a19607845946d0bcc1ed90208d253
Matt <mattbledsoe2@gmail.com>
2017-09-13 10:04:52 -0400

    small changes

---
+++ src/js/model.js
@@ -0,0 +1,137 @@
+
+var breweries = [
+    {
+      name: 'Sugar Creek Brewing Co.',
+      ll: {lat: 35.185429,lng: -80.881015},
+      address: '215 Southside Dr Charlotte, NC 28217',
+      hood: 'Southend',
+      website: 'https://sugarcreekbrewing.com/',
+      style: 'Belgian',
+      icon: '',
+    },
+
+    {
+      name: 'Olde Meck Brewery',
+      lat:35.187449,
+      lng:-80.881815,
+      address: '4150 Yancey Rd Charlotte, NC',
+      hood: 'Southend',
+      website: 'http://www.oldemeckbrew.com/',
+      style: 'German',
+      icon: '',
+
+    },
+    {
+      name: 'Noda Brewing Co.',
+      lat:35.240348,
+      lng:-80.814849,
+      address: '2229 N. Davidson St. Charlotte, NC 28205',
+      hood: 'Noda',
+      website: 'http://nodabrewing.com/',
+      style: 'American',
+      icon: '',
+
+    },
+    {
+      name: 'Salud Cerveceria',
+      lat:35.247799,
+      lng:-80.804048,
+      address: '3306-C N Davidson St, Charlotte, NC 28205',
+      hood: 'Noda',
+      website: 'https://saludcerveceria.com/',
+      style: 'American',
+      icon: '',
+
+    },
+    {
+      name: 'Birdsong Brewing Co.',
+      lat:35.230656,
+      lng:-80.826525,
+      address: '1016 N. Davidson St Charlotte, NC 28206',
+      hood: 'Noda',
+      website: 'https://birdsongbrewing.com/',
+      style: 'American',
+      icon: '',
+
+    },
+    {
+      name: 'Lenny Boy Brewing Co.',
+      lat:35.201444,
+      lng:-80.873595,
+      address: '3000 S. Tryon St Charlotte, NC 28217',
+      hood: 'Southend',
+      website: 'http://www.discoverlennyboy.com/',
+      style: 'American',
+      icon: '',
+
+    },
+    {
+      name: 'Sycamore Brewing',
+      lat:35.208722,
+      lng:-80.862762,
+      address: '2161 Hawkins Street Charlotte, NC, 28203',
+      hood: 'Southend',
+      website: 'https://www.sycamorebrew.com/',
+      style: 'American',
+      id: "545524a1498eb4041c09bf9d",
+      icon: '',
+
+    },
+    {
+      name: 'Unknown Brewing',
+      lat:35.220506,
+      lng:-80.857539,
+      address: '1327 S. Mint St Charlotte, NC 28203',
+      hood: 'Uptown',
+      website: 'http://www.unknownbrewing.com/',
+      style: 'American',
+      id: "5113bb89e4b010ed94f6c1e1",
+      icon: '',
+
+    },
+    {
+      name: 'Triple C Brewing',
+      lat:35.201092,
+      lng:-80.869570,
+      address: '2900 Griffith St, Charlotte, NC 28203',
+      hood: 'Southend',
+      website: 'http://www.triplecbrewing.com/',
+      style: 'American',
+      icon: '',
+
+    },
+    {
+      name: 'Wooden Robot Brewing',
+      lat:35.216677,
+      lng:-80.856644,
+      address: '1440 S Tryon St #110 Charlotte, NC 28203',
+      hood: 'Southend',
+      website: 'https://woodenrobotbrewery.com/',
+      style: 'Belgian',
+      id: "558eea9e498ed2929de8df51",
+      icon: '',
+
+    },
+    {
+      name: 'Legion Brewing',
+      lat:35.218387,
+      lng:-80.812963,
+      address: '1906 Commonwealth Ave Charlotte, NC 28205',
+      hood: 'Plaza Midwood',
+      website: 'http://legionbrewing.com/',
+      style: 'American',
+      icon: '',
+
+    },
+    {
+      name: 'Resident Culture',
+      lat:35.220203,
+      lng:-80.805539,
+      address: '2101 Central Ave Charlotte, NC 28205',
+      hood: 'Plaza Midwood',
+      website: 'https://www.residentculture.com/',
+      style: 'American',
+      icon: '',
+
+    }
+];
