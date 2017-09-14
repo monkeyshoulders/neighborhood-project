5d11e2f349a9653ad6709d3a935eabc0212b0d86
Matt <mattbledsoe2@gmail.com>
2017-09-14 12:56:51 -0400

    Add files via upload

--- Neighborhood Project/src/js/app.js
+++ Neighborhood Project/src/js/app.js
@@ -1,5 +1,37 @@
+var Brewery = function(data) {
+  var self = this;
+  this.name = data.name;
+  this.hood = data.hood + "  | ";
+  this.style = 'Style: ' + data.style;
+  this.website = data.website;
+  // this.ll = data.ll;
+  this.icon = data.icon;
+};
 
-ko.applyBindings(breweries);
+
+
+var ViewModel = function() {
+  var self = this;
+
+  this.breweries = ko.observableArray();
+
+  for (var i = 0; i < brewers.length; i++) {
+    this.breweries.push(new Brewery(brewers[i]));
+  }
+
+
+
+
+  // http://knockoutjs.com/documentation/click-binding.html#note-1-passing-a-current-item-as-a-parameter-to-your-handler-function
+
+  // Filtering an array: http://www.knockmeout.net/2011/04/utility-functions-in-knockoutjs.html
+  // http://knockoutjs.com/documentation/computedObservables.html
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/indexOf
+
+};
+
+ko.applyBindings(new ViewModel());
+
 
 var map;
 
@@ -12,12 +44,31 @@ function initMap() {
     zoom: 13,
     mapTypeControl: false
   });
-}
+  // markers
+  for (var i = 0; i < brewers.length; i++) {
+         var position = brewers[i].ll;
+         var marker = new google.maps.Marker({
+           position: position,
+           map: map,
+           icon: brewers[i].mapicon,
+           animation: google.maps.Animation.DROP,
+         });
+         markers.push(marker);
 
-var Marker = function() {
+         markers.addListener('click', function() {
+            infoWindow.open(map, marker);
+          });
 
-}
+        }
 
-var InfoWindow = function() {
+    var window = function(){
+      for (var i = 0; i < brewers.length; i++) {
+        var info = '<h3>brewers[i].name</h3>';
+      }
+    };
+
+    var infoWindow = new google.maps.InfoWindow({
+      content: window
+    });
 
 }
--- Neighborhood Project/src/js/model.js
+++ Neighborhood Project/src/js/model.js
@@ -1,169 +1,161 @@
-// var map;
-// function initMap() {
-//  map = new google.maps.Map(document.getElementById('map'), {
-//    center: {lat: 35.223108, lng: -80.754541},
-//    zoom: 7
-//  });
-//
-//  var home = {lat: 35.223108, lng: -80.754541};
-//  var marker1 = new google.maps.Marker({
-//    position: home,
-//    map: map,
-//    title: 'uhhhhhhhh!'
-//  });
-//  var infowindow1 = new google.maps.InfoWindow({
-//    content: 'Why your wife live here?'
-//  });
-//  marker1.addListener('click', function(){
-//    infowindow1.open(map, marker1);
-//  });
-//
-//  var paradise = {lat: 35.844107, lng: -75.562205};
-//  var marker = new google.maps.Marker({
-//    position: paradise,
-//    map: map,
-//    title: 'Paradise!'
-//  });
-//  var infowindow = new google.maps.InfoWindow({
-//    content: 'Why your wife no live here?'
-//  });
-//  marker.addListener('click', function(){
-//    infowindow.open(map, marker);
-//  });
-// }
-
-
-var breweries = [
-    {
-      name: 'Sugar Creek Brewing Co.',
+var brewers = [{
+    name: 'Sugar Creek Brewing Co.',
+    ll: {
       lat: 35.185429,
-      lng: -80.881015,
-      address: '215 Southside Dr Charlotte, NC 28217',
-      hood: 'Southend',
-      website: 'https://sugarcreekbrewing.com/',
-      style: 'Belgian',
-      icon: '',
+      lng: -80.881015
     },
+    address: '215 Southside Dr',
+    hood: 'Southend',
+    website: 'https://sugarcreekbrewing.com/',
+    style: 'Belgian',
+    icon: 'img/sugarcreek.jpg',
+    mapicon: 'img/sugarcreekmap.png'
+  },
 
-    {
-      name: 'Olde Meck Brewery',
-      lat:35.187449,
-      lng:-80.881815,
-      address: '4150 Yancey Rd Charlotte, NC',
-      hood: 'Southend',
-      website: 'http://www.oldemeckbrew.com/',
-      style: 'German',
-      icon: '',
-
+  {
+    name: 'Olde Meck Brewery',
+    ll: {
+      lat: 35.187449,
+      lng: -80.881815
     },
-    {
-      name: 'Noda Brewing Co.',
-      lat:35.240348,
-      lng:-80.814849,
-      address: '2229 N. Davidson St. Charlotte, NC 28205',
-      hood: 'Noda',
-      website: 'http://nodabrewing.com/',
-      style: 'American',
-      icon: '',
-
+    address: '4150 Yancey Rd',
+    hood: 'Southend',
+    website: 'http://www.oldemeckbrew.com/',
+    style: 'German',
+    icon: 'img/omb.jpg',
+    mapicon: 'img/ombmap.png'
+  },
+  {
+    name: 'Noda Brewing Co.',
+    ll: {
+      lat: 35.240348,
+      lng: -80.814849
     },
-    {
-      name: 'Salud Cerveceria',
-      lat:35.247799,
-      lng:-80.804048,
-      address: '3306-C N Davidson St, Charlotte, NC 28205',
-      hood: 'Noda',
-      website: 'https://saludcerveceria.com/',
-      style: 'American',
-      icon: '',
-
+    address: '2229 N. Davidson St',
+    hood: 'Noda',
+    website: 'http://nodabrewing.com/',
+    style: 'American',
+    icon: 'img/noda.png',
+    mapicon: 'img/nodamap.png'
+  },
+  {
+    name: 'Salud Cerveceria',
+    ll: {
+      lat: 35.247799,
+      lng: -80.804048
     },
-    {
-      name: 'Birdsong Brewing Co.',
-      lat:35.230656,
-      lng:-80.826525,
-      address: '1016 N. Davidson St Charlotte, NC 28206',
-      hood: 'Noda',
-      website: 'https://birdsongbrewing.com/',
-      style: 'American',
-      icon: '',
-
+    address: '3306-C N Davidson St',
+    hood: 'Noda',
+    website: 'https://saludcerveceria.com/',
+    style: 'American',
+    icon: 'img/salud.jpg',
+    mapicon: 'img/saludmap.png'
+  },
+  {
+    name: 'Birdsong Brewing Co.',
+    ll: {
+      lat: 35.230656,
+      lng: -80.826525
     },
-    {
-      name: 'Lenny Boy Brewing Co.',
-      lat:35.201444,
-      lng:-80.873595,
-      address: '3000 S. Tryon St Charlotte, NC 28217',
-      hood: 'Southend',
-      website: 'http://www.discoverlennyboy.com/',
-      style: 'American',
-      icon: '',
-
+    address: '1016 N. Davidson St',
+    hood: 'Noda',
+    website: 'https://birdsongbrewing.com/',
+    style: 'American',
+    icon: 'img/birdsong.jpg',
+    mapicon: 'img/birdsongmap.png'
+  },
+  {
+    name: 'Lenny Boy Brewing Co.',
+    ll: {
+      lat: 35.201444,
+      lng: -80.873595
     },
-    {
-      name: 'Sycamore Brewing',
-      lat:35.208722,
-      lng:-80.862762,
-      address: '2161 Hawkins Street Charlotte, NC, 28203',
-      hood: 'Southend',
-      website: 'https://www.sycamorebrew.com/',
-      style: 'American',
-      icon: '',
-
+    address: '3000 S. Tryon St',
+    hood: 'Southend',
+    website: 'http://www.discoverlennyboy.com/',
+    style: 'American',
+    icon: 'img/lenny.jpg',
+    mapicon: 'img/lennymap.png'
+  },
+  {
+    name: 'Sycamore Brewing',
+    ll: {
+      lat: 35.208722,
+      lng: -80.862762
     },
-    {
-      name: 'Unknown Brewing',
-      lat:35.220506,
-      lng:-80.857539,
-      address: '1327 S. Mint St Charlotte, NC 28203',
-      hood: 'Uptown',
-      website: 'http://www.unknownbrewing.com/',
-      style: 'American',
-      icon: '',
-
+    address: '2161 Hawkins Street',
+    hood: 'Southend',
+    website: 'https://www.sycamorebrew.com/',
+    style: 'American',
+    id: "545524a1498eb4041c09bf9d",
+    icon: 'img/sycamore.jpg',
+    mapicon: 'img/sycamoremap.png'
+  },
+  {
+    name: 'Unknown Brewing',
+    ll: {
+      lat: 35.220506,
+      lng: -80.857539
     },
-    {
-      name: 'Triple C Brewing',
-      lat:35.201092,
-      lng:-80.869570,
-      address: '2900 Griffith St, Charlotte, NC 28203',
-      hood: 'Southend',
-      website: 'http://www.triplecbrewing.com/',
-      style: 'American',
-      icon: '',
-
+    address: '1327 S. Mint St',
+    hood: 'Uptown',
+    website: 'http://www.unknownbrewing.com/',
+    style: 'American',
+    id: "5113bb89e4b010ed94f6c1e1",
+    icon: 'img/unknown.jpg',
+    mapicon: 'img/unknownmap.png'
+  },
+  {
+    name: 'Triple C Brewing',
+    ll: {
+      lat: 35.201092,
+      lng: -80.869570
     },
-    {
-      name: 'Wooden Robot Brewing',
-      lat:35.216677,
-      lng:-80.856644,
-      address: '1440 S Tryon St #110 Charlotte, NC 28203',
-      hood: 'Southend',
-      website: 'https://woodenrobotbrewery.com/',
-      style: 'Belgian',
-      icon: '',
-
+    address: '2900 Griffith St',
+    hood: 'Southend',
+    website: 'http://www.triplecbrewing.com/',
+    style: 'American',
+    icon: 'img/tripc.jpg',
+    mapicon: 'img/tripcmap.png'
+  },
+  {
+    name: 'Wooden Robot Brewing',
+    ll: {
+      lat: 35.216677,
+      lng: -80.856644
     },
-    {
-      name: 'Legion Brewing',
-      lat:35.218387,
-      lng:-80.812963,
-      address: '1906 Commonwealth Ave Charlotte, NC 28205',
-      hood: 'Plaza Midwood',
-      website: 'http://legionbrewing.com/',
-      style: 'American',
-      icon: '',
-
+    address: '1440 S Tryon St #110',
+    hood: 'Southend',
+    website: 'https://woodenrobotbrewery.com/',
+    style: 'Belgian',
+    id: "558eea9e498ed2929de8df51",
+    icon: 'img/wooden.jpeg',
+    mapicon: 'img/woodenmap.png'
+  },
+  {
+    name: 'Legion Brewing',
+    ll: {
+      lat: 35.218387,
+      lng: -80.812963
     },
-    {
-      name: 'Resident Culture',
-      lat:35.220203,
-      lng:-80.805539,
-      address: '2101 Central Ave Charlotte, NC 28205',
-      hood: 'Plaza Midwood',
-      website: 'https://www.residentculture.com/',
-      style: 'American',
-      icon: '',
-
-    }
+    address: '1906 Commonwealth Ave',
+    hood: 'Plaza Midwood',
+    website: 'http://legionbrewing.com/',
+    style: 'American',
+    icon: 'img/legion.jpg',
+    mapicon: 'img/legionmap.png'
+  },
+  {
+    name: 'Resident Culture',
+    ll: {
+      lat: 35.220203,
+      lng: -80.805539
+    },
+    address: '2101 Central Ave',
+    hood: 'Plaza Midwood',
+    website: 'https://www.residentculture.com/',
+    style: 'American',
+    icon: 'img/resident.jpeg',
+    mapicon: 'img/residentmap.png'
+  }
 ];
