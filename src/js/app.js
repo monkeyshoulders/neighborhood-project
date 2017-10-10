8b794624a34b494c070d04518b3a8a29ebe44906
Matt Bledsoe <mattbledsoe@Matts-MacBook-Pro.local>
2017-10-10 18:28:02 -0400

    toggle sidebar

--- src/js/app.js
+++ src/js/app.js
@@ -30,6 +30,16 @@
 //
 // //request.send();
 
+function toggleSidebar() {
+  var x = document.getElementById("sidebar");
+   if (x.style.display === "none") {
+       x.style.display = "block";
+   } else {
+       x.style.display = "none";
+       document.getElementById('map').style.width = "100%";
+   }
+}
+
 
 
 function getData(city, query, data) {
